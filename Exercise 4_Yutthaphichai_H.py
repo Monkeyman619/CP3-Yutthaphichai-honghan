44d9d9438dd968e81d17b22ea21da33fb603ce41
Monkeyman619 <yutthaphichai.hong@gmail.com>
2022-11-29 17:24:41 +0700

    Create Exercise 4_Yutthaphichai_H.py

---
+++ Exercise 4_Yutthaphichai_H.py
@@ -0,0 +1,10 @@
+F = 62.1 # Foundation English Score
+G = 71.4 # General business
+I = 99.3 # Introduction to Computer Science
+C = 87.5 # Computer programming
+
+print("---Your Score---");
+print("Foundation English Score :",F);
+print("General business Score :",G);
+print("Introduction to Computer Science Score :",I);
+print("Computer programming",C);
